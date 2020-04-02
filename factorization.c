6e2e160f78c9cfea3854048b97a7354b72b8ebfb
hjeon <hjeon@student.42seoul.kr>
2020-04-02 11:23:52 +0900

    update validation feature

--- factorization.c
+++ factorization.c
@@ -12,15 +12,19 @@
 
 #include <unistd.h>
 
+#define	TRUE	1
+#define	FALSE	0
 #define	ERROR	-1
+#define	t_bool	int
 
 void			print_str(char *str);
 void			print_int(unsigned int number);
 void			print_prime_factors(unsigned int number);
 int				arg_check(int argc, char *argv[]);
-int				is_prime(unsigned int nbr);
 int				validate_string(char *str);
-unsigned int	simple_atoi(char *str);
+unsigned int	simple_atoui(char *str);
+t_bool			is_prime(unsigned int number);
+t_bool			is_factor(unsigned int candidate, unsigned int number);
 
 int				main(int argc, char *argv[])
 {
@@ -28,66 +32,51 @@ int				main(int argc, char *argv[])
 
 	if (arg_check(argc, argv) == ERROR)
 		return (-1);
-	number = simple_atoi(argv[1]);
+	number = simple_atoui(argv[1]);
 	print_prime_factors(number);
 	return (0);
 }
 
 int				arg_check(int argc, char *argv[])
 {
-	if (argc != 2)
-		print_str("Please input just 1 argument.");
+	if (argc == 1)
+		print_str("Please enter a argument.\n");
+	else if (2 < argc)
+		print_str("Too many arguments. Please enter just 1 argument.\n");
 	else if (validate_string(argv[1]) == ERROR)
-		print_str("Please enter only numbers");
-	else if (simple_atoi(argv[1]) < 2)
-		print_str("Please enter a valid range. (2 ~ 4294967295)");
+		print_str("Please enter only numbers\n");
+	else if (simple_atoui(argv[1]) < 2)
+		print_str("Please enter a valid range. (2 ~ 4294967295)\n");
 	else
-		return (1);
+		return (0);
 	return (ERROR);
 }
 
 void			print_prime_factors(unsigned int number)
 {
-	unsigned int	factor;
+	unsigned int	i;
 
-	factor = 2;
-	while (number != 1)
+	i = 2;
+	while (!is_prime(number))
 	{
-		if (number % factor == 0)
+		if (is_factor(i, number))
 		{
-			print_int(factor);
-			while (number % factor == 0)
-				number /= factor;
-			if (number != 1)
-				print_str(" ");
+			print_int(i);
+			while (is_factor(i, number))
+				number /= i;
+			if (number == 1)
+				return ;
+			print_str(" ");
 		}
-		else if (is_prime(number))
-		{
-			print_int(number);
-			return ;
-		}
-		factor++;
+		i++;
 	}
+	print_int(number);
+	return ;
 }
 
 /*
 ** Utils
 */
-
-int				is_prime(unsigned int nbr)
-{
-	unsigned int i;
-
-	i = 2;
-	while (i * i <= nbr)
-	{
-		if (nbr % i == 0){
-			return (0);
-		}
-		i++;
-	}
-	return (1);
-}
 
 void			print_int(unsigned int number)
 {
@@ -99,11 +88,21 @@ void			print_int(unsigned int number)
 	return ;
 }
 
+void			print_str(char *str)
+{
+	int		str_len;
+
+	str_len = 0;
+	while (*(str + str_len))
+		str_len++;
+	write(1, str, str_len);
+}
+
 int				validate_string(char *str)
 {
 	while (('\t' <= *str && *str <= '\r') || *str == ' ')
 		str++;
-	if (*str == '+')
+	if (*str == '+' || *str == '-')
 		str++;
 	if (*str < '0' && '9' < *str)
 		return (ERROR);
@@ -116,32 +115,52 @@ int				validate_string(char *str)
 	return (0);
 }
 
-unsigned int	simple_atoi(char *str)
+unsigned int	simple_atoui(char *str)
 {
-	unsigned int		i;
-	unsigned int		result;
+	unsigned int	i;
+	unsigned long	result;
+	t_bool			is_signed;
 
-	i = 0;
 	result = 0;
+	is_signed = FALSE;
 	while (('\t' <= *str && *str <= '\r') || *str == ' ')
 		str++;
-	if (*str == '+')
-		str++;
-	while ('0' <= *str && *str <= '9')
+	if (*str == '+' || *str == '-')
 	{
-		result *= 10;
-		result += *str - '0';
+		if (*str == '-')
+			is_signed = TRUE;
 		str++;
 	}
+	i = 0;
+	while ('0' <= *(str + i) && *(str + i) <= '9')
+	{
+		result *= 10;
+		result += *(str + i) - '0';
+		i++;
+	}
+	if (10 < i || is_signed)
+		result = 0;
 	return (result);
 }
 
-void			print_str(char *str)
+t_bool			is_prime(unsigned int number)
 {
-	int		str_len;
+	unsigned long i;
 
-	str_len = 0;
-	while (*(str + str_len))
-		str_len++;
-	write(1, str, str_len);
+	i = 2;
+	while (i * i <= number)
+	{
+		if (is_factor(i, number))
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
+}
+
+t_bool			is_factor(unsigned int candidate, unsigned int number)
+{
+	if (number % candidate == 0)
+		return (TRUE);
+	else
+		return (FALSE);
 }
